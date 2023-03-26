37d1bbaf1c0366b63b2d08dd10fac33db17e81e3
Sergio O. Ayala <sergio.programador@outlook.com>
2023-03-26 21:40:09 +0200

    Clean

--- FuncPdfConvert/ConvertToPdf.cs
+++ FuncPdfConvert/ConvertToPdf.cs
@@ -15,44 +15,50 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Graph;
 using Microsoft.Graph.Models;
 using Microsoft.Graph.Models.ODataErrors;
+using Microsoft.Kiota.Abstractions;
 
 namespace FuncPdfConvert
 {
     internal class ConvertToPdf
     {
         private readonly IConfiguration configuration;
+
         public ConvertToPdf(IConfiguration configuration)
         {
             this.configuration = configuration;
-
         }
 
         [FunctionName("ConvertToPdf")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]
+                HttpRequest req,
+            ILogger log
+        )
         {
             try
             {
-                string tmpFileName = $"{Guid.NewGuid()}{MimeTypes.MimeTypeMap.GetExtension(req.ContentType)}";
+                string tmpFileName =
+                    $"{Guid.NewGuid()}{MimeTypes.MimeTypeMap.GetExtension(req.ContentType)}";
                 GraphServiceClient client = TokenAuth();
 
                 // UPLOAD FILE
                 DriveCollectionResponse drives = await client!.Drives.GetAsync();
                 Drive drive = drives.Value.FirstOrDefault();
-                await client.Drives[drive.Id].Root.ItemWithPath(tmpFileName)
-                      .Content.PutAsync(req.Body, null, CancellationToken.None);
+                await client.Drives[drive.Id].Root
+                    .ItemWithPath(tmpFileName)
+                    .Content.PutAsync(req.Body, null, CancellationToken.None);
                 log.LogInformation("Upload to folder: {a}", drive.Name);
 
                 // DOWNLOAD FILE AS PDF
-                Microsoft.Kiota.Abstractions.RequestInformation requestInformation = client.Drives[drive.Id].Root
+                RequestInformation requestInformation = client.Drives[drive.Id].Root
                     .ItemWithPath(Path.GetFileName(tmpFileName))
                     .Content.ToGetRequestInformation();
                 requestInformation.UrlTemplate += "{?format}";
                 requestInformation.QueryParameters.Add("format", "pdf");
-                Stream stream = await client.RequestAdapter.SendPrimitiveAsync<Stream>(requestInformation);
+                Stream stream = await client.RequestAdapter.SendPrimitiveAsync<Stream>(
+                    requestInformation
+                );
                 return new OkObjectResult(stream);
-
             }
             catch (ODataError odataError)
             {
@@ -62,20 +68,18 @@ namespace FuncPdfConvert
             }
         }
 
-
         public GraphServiceClient TokenAuth()
         {
             string[] scopes = { "https://graph.microsoft.com/.default" };
             ClientSecretCredential clientSecretCredential =
-            new(
+                new(
                     configuration.GetValue<string>("ADConfig:TenantId"),
-                   configuration.GetValue<string>("ADConfig:ClientId"),
+                    configuration.GetValue<string>("ADConfig:ClientId"),
                     configuration.GetValue<string>("ADConfig:ClientSecret"),
                     new() { AuthorityHost = AzureAuthorityHosts.AzurePublicCloud }
                 );
             GraphServiceClient graphClient = new(clientSecretCredential, scopes);
             return graphClient;
         }
-
     }
 }
